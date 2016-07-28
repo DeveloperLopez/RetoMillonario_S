2402cf33b51a2d333723d661964c5d6a0c08ec35
Fernando López <fernando@developerlopez.com>
2016-07-28 02:51:11 -0500

    Se agrega facebook id

--- facebook.php
+++ facebook.php
@@ -5,15 +5,16 @@ $email = $_POST['email'];
 $password = $_POST['pwd'];
 $name = $_POST['name'];
 $pwd = password_hash($password, PASSWORD_BCRYPT);
+$id_facebook = $_POST['pwd'];
 $jsondata = array();
 
-$searchUser = "SELECT * FROM $tbl_name WHERE email = '$email' ";
+$searchUser = "SELECT * FROM $tbl_name WHERE email = '$email' OR id_facebook = '$id_facebook'";
 $result = $connection->query($searchUser);
 $count = mysqli_num_rows($result);
 
 if ($count == 1) {
 
-    $sql = "SELECT * FROM $tbl_name WHERE email = '$email'";
+    $sql = "SELECT * FROM $tbl_name WHERE email = '$email' OR id_facebook = '$id_facebook'";
     $result = $connection->query($sql);
     $row = $result->fetch_array(MYSQLI_ASSOC);
 
@@ -21,9 +22,9 @@ if ($count == 1) {
     $jsondata['id']      = $row['id'];
     $jsondata['email']   = $row['email'];
 } else {
-    $query = "INSERT INTO $tbl_name (email, password, name) VALUES ('$email', '$pwd', '$name')";
+    $query = "INSERT INTO $tbl_name (email, password, name, id_facebook) VALUES ('$email', '$pwd', '$name', '$id_facebook')";
     if ($connection->query($query) === TRUE) {
-        $sql = "SELECT * FROM $tbl_name WHERE email = '$email'";
+        $sql = "SELECT * FROM $tbl_name WHERE id_facebook = '$id_facebook'";
         $result = $connection->query($sql);
         $row = $result->fetch_array(MYSQLI_ASSOC);
 
